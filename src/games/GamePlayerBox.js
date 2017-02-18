d39664734c64ce25a773013ed838ea478c4507aa
Melvin Zehl <mhzehl@gmail.com>
2017-02-18 17:45:12 +0100

    Create GamePlayerBox

--- src/games/GamePlayerBox.js
+++ src/games/GamePlayerBox.js
@@ -1,7 +1,25 @@
-// import React, { PureComponent } from 'react'
-//
-// export default class GamePlayerBox extends PureComponent {
-//   render() {
-//     return null
-//   }
-// }
+import React, { PureComponent } from 'react'
+import PlayerAvatar from '../components/PlayerAvatar'
+import GameScore from './GameScore'
+import RaisedButton from 'material-ui/RaisedButton'
+import './GamePlayerBox.sass'
+
+export default class GamePlayerBox extends PureComponent {
+  render() {
+    return(
+      <div className="playerbox">
+        <div className="player">
+          <div className="avatar">
+            <PlayerAvatar />
+          </div>
+          <div className="ready">
+            <RaisedButton type="submit" label="I'm Ready!" />
+          </div>
+        </div>
+        <div className="score">
+          <GameScore />
+        </div>
+      </div>
+    )
+  }
+}
