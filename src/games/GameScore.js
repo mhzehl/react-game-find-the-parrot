c0b0f11c323e91da086eaf83ca424eb55c44c6cf
Melvin Zehl <mhzehl@gmail.com>
2017-02-18 17:44:38 +0100

    Create game score component

---
+++ src/games/GameScore.js
@@ -0,0 +1,25 @@
+import React, { PureComponent } from 'react'
+import Paper from 'material-ui/Paper';
+import './GameScore.sass'
+
+const style = {
+  height:60,
+  width: 60,
+  margin: 20,
+  textAlign: 'center',
+  display: 'inline-block',
+};
+
+export default class GameScore extends PureComponent {
+  render() {
+    return(
+      <div>
+        <Paper style={style} zDepth={4}>
+          <div className="playerScore">
+            5
+          </div>
+        </Paper>
+      </div>
+    )
+  }
+}
