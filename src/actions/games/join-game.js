8f7f12d09dc9d69700c1f34c0eb218bc905ac29a
Melvin Zehl <mhzehl@gmail.com>
2017-02-21 08:44:59 +0100

    Route to game page with corresponding id

--- src/actions/games/join-game.js
+++ src/actions/games/join-game.js
@@ -1,14 +1,21 @@
 import API from '../../middleware/api'
 const api = new API()
 const games = api.service('games')
+import { history } from '../../store'
 
 export default (game, user) => {
   const players = game.players
 
-  console.log('JoinGame api: ', game.players )
+  console.log('JoinGame api: ', game._id )
   api.app.authenticate()
     .then(() => {
       games.update(game._id, { ...players[user._id] })
+      .then(() => {
+        history.push('/games/' + game._id)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
     })
     .catch((error) => {
       console.error(error)
